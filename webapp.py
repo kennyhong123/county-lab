729697c2291b9f4ff19911ebb21623fab141c3d9
kennyhong123 <31251440+kennyhong123@users.noreply.github.com>
2017-11-09 09:20:09 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,16 +1,37 @@
-from flask import Flask, url_for, render_template, request, markups
+from flask import Flask, url_for, render_template, request, Markup, flash
+import os
+import json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
+with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+
 def get_state_options():
-    op = Markup("<option value="memes">" + "memes" + "</option>")
-    return op
+    state = counties[0]["State"]
+    choice = ""
+    for c in counties:
+        if state != c["State"]:
+            choice += Markup("<option value=" + state +">" + state + "</option>")
+            state = c["State"]
+    return choice
+
+def get_statefact(state):
+    fact = 0
+    for c in counties:
+        if state == c["State"]:
+            fact += c["Employment"]["Firms"]["Total"]
+    funfact = Markup("<p>" + "Number of People Employed in " + state + " is, " + str(fact) + "</p>")
+    return funfact
 
 @app.route("/")
 def render_main():
-    get_state_options()
-    return render_template('home.html', option = get_state_options())
-    
-    
+   return render_template('index.html', option = get_state_options())
+
+@app.route("/app", methods=['GET','POST'])
+def render_fact():
+    area = request.args['dat']
+    return render_template('index.html', fact = get_statefact(area), option = get_state_options())
+
 if __name__=="__main__":
-    app.run(debug=False, port=54321)
+     app.run(debug=False, port=54321)
